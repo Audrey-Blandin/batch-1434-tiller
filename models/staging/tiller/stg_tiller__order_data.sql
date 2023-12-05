9e19f30a50090df6ba638dc090abd6f422c47f46
Georges BOURGUES <tillerbysumup@gmail.com>
2023-12-05 11:52:25 +0000

    correcting schema cleaning order data (null values)

--- models/staging/tiller/stg_tiller__order_data.sql
+++ models/staging/tiller/stg_tiller__order_data.sql
@@ -11,13 +11,13 @@ renamed as (
     select
         id_order,
         id_store,
-        id_table,
-        id_waiter,
-        id_device,
+        CASE WHEN id_table IS NULL THEN 0 ELSE id_table END AS id_table,
+        CASE WHEN id_waiter IS NULL THEN 0 ELSE id_waiter END AS id_waiter,
+        CASE WHEN id_device IS NULL THEN 0 ELSE id_device END AS id_device,
         date_opened,
         date_closed,
         dim_status,
-        dim_source,
+        CASE WHEN dim_source IS NULL THEN 'Other' ELSE dim_source END AS dim_source,
         m_nb_customer,
         m_cached_payed,
         m_cached_price
