1dbea509d1113549bd330704dcea8ed50a5400b6
Audrey Blandin <audrey.blandin@gmail.com>
2023-12-07 15:48:19 +0000

    Modification par un INNER JOIN

--- models/intermediate/int_product_performance.sql
+++ models/intermediate/int_product_performance.sql
@@ -11,5 +11,5 @@ select
     order_line.m_unit_price,
     order_line.m_total_price_inc_vat
 FROM {{ref("stg_tiller__order_data")}} order_data
-LEFT JOIN {{ref("stg_tiller__order_line")}} order_line
+INNER JOIN {{ref("stg_tiller__order_line")}} order_line
 USING (id_order)
